3e2fbabff3de652fe4830ebe29ce5f8aab0538da
AIDEVEYSBD <jai.verma2@in.ey.com>
2025-09-04 14:45:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -487,6 +487,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8000)),
+        port=int(os.environ.get("PORT", 8080)),
         reload=os.environ.get("RELOAD", "false").lower() == "true",
     )
+
